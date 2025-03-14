12be4cf4e042f5b18ab14913f073478a33a47cd5
Aymen Ebrahim <aymenebrahim15@gmail.com>
2025-03-14 10:44:24 +0100

    Feat: temporarily disabled caching for fetching from PokeAPI

--- app/services/pokemonService.tsx
+++ app/services/pokemonService.tsx
@@ -2,14 +2,22 @@ import { PokemonListResponse, PokemonDetails } from "../types/pokemon";
 
 const BASE_URL = 'https://pokeapi.co/api/v2';
 
-export const getPokemonList = async (limit = 12, offset = 0): Promise<PokemonListResponse> => {
-    const response = await fetch(`${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`);
+export const getPokemonList = async (limit = 1, offset = 0): Promise<PokemonListResponse> => {
+    const response = await fetch(`${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`, {
+        headers: {
+            'Cache-Control': 'no-cache'
+        }
+    });
     if (!response.ok) {throw new Error('Failed to fetch');}
     return response.json();
 };
 
 export const getPokemonDetails = async (nameOrId: string | number): Promise<PokemonDetails> => {
-    const response = await fetch(`${BASE_URL}/pokemon/${nameOrId}`);
+    const response = await fetch(`${BASE_URL}/pokemon/${nameOrId}`, {
+        headers: {
+            'Cache-Control': 'no-cache'
+        }
+    });
     if (!response.ok) {throw new Error('Failed to fetch');}
     return response.json();
 }
